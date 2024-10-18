4442f4073a45d630562cdc96f716860d3123c6f3
jsparrowio <jsparrowio@outlook.com>
2024-10-18 11:40:10 -0700

    challenge code written and reached MVP

---
+++ GeneratedREADME.md
@@ -0,0 +1,50 @@
+# README Generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+                 
+## Description
+  
+A node.js program that you can use to generate a professional README for your coding project.
+  
+## Table of Contents
+  
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Credits](#credits)
+- [Tests](#tests)
+- [Questions](#questions)
+  
+## Installation
+  
+Download the source code for the project using a git clone on this repository. Then, CD in your terminal to the directory that contains the cloned repo, and run an npm i to ensure that you have all required dependencies to run the package successfully.
+  
+## Usage
+  
+To use the program successfully: open a terminal window and CD to the directory that contains the code (specifically index.js) for this program. Then run <code>node index.js</code>, which will then start to ask you a series of questions about your project to be included in your README file. Then, once you answer all questions, the program will then output a README file into the directory you are in labeled GeneratedREADME.md which you can then use for your project.
+
+## License
+This project uses MIT License.
+
+Please refer to LICENSE file for more information.
+ 
+## Contributing
+  
+No need to contribute as the project is completed. Feel free to fork to make it your own!
+  
+## Credits
+  
+Base code provided by edX and their respective teams, additional written code by jsparrowio (Josh Garrett)
+  
+## Tests
+  
+To test, follow installation and usage instructions above.
+  
+## Questions
+  
+If you have any additional questions, please contact me at:
+  
+[GitHub](https://www.github.com/jsparrowio)
+  
+[jsparrowio@outlook.com](mailto:jsparrowio@outlook.com)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,106 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseBadge(license) {
+  let licenseBadge;
+  if (license === "Apache 2.0 License") {
+      licenseBadge = `
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+      `;
+  } else if (license === "MIT License") {
+      licenseBadge = `
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+      `;
+  } else if (license === "GNU GPL v3 License") {
+      licenseBadge = `
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+      `;
+  } else if (license === "BSD 3-Clause License")  {
+      licenseBadge = `
+[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+      `;
+  } else {
+      licenseBadge = " ";
+  }
+  return licenseBadge;
 }
 
+// Function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license !== 'No License'){
+  const licenseLink = `
+- [License](#license)`;
+  return licenseLink;
+  } else {
+      const licenseLink = '';
+      return licenseLink;
+  }
+}
+
+// Function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== 'No License'){
+  const licenseSection = `
+## License
+This project uses ${license}.
+
+Please refer to LICENSE file for more information.
+`
+return licenseSection;} else {
+    const licenseSection = '';
+    return licenseSection;
+}
+}
+
+// Function to generate markdown for README
+function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+  return `# ${data.title}
+${licenseBadge}           
+## Description
+  
+${data.description}
+  
+## Table of Contents
+  
+- [Installation](#installation)
+- [Usage](#usage)${licenseLink}
+- [Contributing](#contributing)
+- [Credits](#credits)
+- [Tests](#tests)
+- [Questions](#questions)
+  
+## Installation
+  
+${data.installation}
+  
+## Usage
+  
+${data.usage}
+${licenseSection} 
+## Contributing
+  
+${data.contributing}
+  
+## Credits
+  
+${data.credits}
+  
+## Tests
+  
+${data.tests}
+  
+## Questions
+  
+If you have any additional questions, please contact me at:
+  
+[GitHub](https://www.github.com/${data.github})
+  
+[${data.email}](mailto:${data.email})`;
+}
+
+// Exports module for import
 export default generateMarkdown;
